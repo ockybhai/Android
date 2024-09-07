c383fbf7e693140523483b0dfad7a7f39a484352
ockybhai <146436540+ockybhai@users.noreply.github.com>
2024-09-07 11:34:41 +0530

    Update MainActivity.java

--- background/MainActivity.java
+++ background/MainActivity.java
@@ -1,1 +1,36 @@
+package com.example.background;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ConstraintLayout lay;
+    int r=0,g=0,b=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        lay=findViewById(R.id.main);
+    }
+
+    public void colorr(View view) {
+        r=(r+10)%256;
+        g=(g+20)%256;
+        b=(b+30)%256;
+        int colorr= Color.rgb(r,g,b);
+        lay.setBackgroundColor(colorr);
+    }
+}
